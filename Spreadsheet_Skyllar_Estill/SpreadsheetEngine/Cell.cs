0003e222c61f1126a2d8eea53c0c9e4362770928
skyllare <skyllarautumn@gmail.com>
2023-02-22 22:16:26 -0800

    column and row index in DLL

--- Spreadsheet_Skyllar_Estill/SpreadsheetEngine/Cell.cs
+++ Spreadsheet_Skyllar_Estill/SpreadsheetEngine/Cell.cs
@@ -9,7 +9,43 @@ namespace SpreadsheetEngine
     /// </summary>
     public class Cell
     {
-       
+        /// <summary>
+        /// private read-only value for the row index.
+        /// </summary>
+        private readonly int rowIndex;
+
+        /// <summary>
+        /// private read-only value for the column index.
+        /// </summary>
+        private readonly int columnIndex;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class.
+        /// </summary>
+        /// <param name="inputRowIndex">what row</param>
+        /// <param name="inputColumnIndex">what column</param>
+        private Cell(int inputRowIndex, int inputColumnIndex)
+        {
+            this.rowIndex = inputRowIndex;
+            this.columnIndex = inputColumnIndex;
+        }
+
+        /// <summary>
+        /// Gets the row index of this cell.
+        /// </summary>
+        public int RowIndex
+        {
+            get { return this.rowIndex; }
+        }
+
+        /// <summary>
+        /// Gets the column index of this cell.
+        /// </summary>
+        public int ColumnIndex
+        {
+            get { return this.columnIndex; }
+        }
+    }
 
 }
 
