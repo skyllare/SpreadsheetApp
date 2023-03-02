888befc4bfcf0be9c0be7dfd55b6fa23a1c7d067
skyllare <skyllarautumn@gmail.com>
2023-03-01 21:49:22 -0800

    classes, DLL, Stylecop

---
+++ Spreadsheet_Skyllar_Estill/SpreadsheetTesting/SpreadsheetTest.cs
@@ -0,0 +1,16 @@
+namespace SpreadsheetTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Spreadsheet_Skyllar_Estill/Spreadsheet_Skyllar_Estill/Form1.cs
@@ -0,0 +1,10 @@
+namespace Spreadsheet_Skyllar_Estill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
