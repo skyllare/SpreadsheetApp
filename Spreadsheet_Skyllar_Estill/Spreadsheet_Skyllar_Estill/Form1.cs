dc854e735f6e048fec1c4e066e61ee7f86707c38
skyllare <skyllarautumn@gmail.com>
2023-03-01 21:50:40 -0800

    rows and columns

--- Spreadsheet_Skyllar_Estill/Spreadsheet_Skyllar_Estill/Form1.cs
+++ Spreadsheet_Skyllar_Estill/Spreadsheet_Skyllar_Estill/Form1.cs
@@ -1,3 +1,4 @@
+using SpreadsheetEngine;
 namespace Spreadsheet_Skyllar_Estill
 {
     public partial class Form1 : Form
@@ -5,6 +6,31 @@ namespace Spreadsheet_Skyllar_Estill
         public Form1()
         {
             InitializeComponent();
+            InitializeDataGrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// adds rows 1-50 and columns A-Z to the spreadsheet.
+        /// </summary>
+        private void InitializeDataGrid()
+        {
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                this.dataGridView1.Columns.Add(new DataGridViewColumn() { HeaderText = i.ToString(), CellTemplate = new DataGridViewTextBoxCell() });
+            }
+
+            for (int i = 1; i <= 50; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(this.dataGridView1);
+                row.HeaderCell.Value = i.ToString();
+                this.dataGridView1.Rows.Add(row);
+            }
         }
     }
 }
