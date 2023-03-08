2b2db50dc763815d4148fa7f8a0b20c66c59f4ec
skyllare <skyllarautumn@gmail.com>
2023-03-08 10:45:16 -0800

    project removed

--- HW5-ExpressionTree/ExpressionTreeTests/Usings.cs
+++
@@ -1,5 +0,0 @@
-// <copyright file="Usings.cs" company="Skyllar Estil">
-// Copyright (c) Skyllar Estill. All rights reserved.
-// </copyright>
-
-global using NUnit.Framework;
--- HW5-ExpressionTree/HW5-ExpressionTree/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// <copyright file="Program.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-Console.WriteLine("Hello, World!");
