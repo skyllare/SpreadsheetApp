43d1672af06e21a57d90fa21cb2887915a7e50ce
skyllare <skyllarautumn@gmail.com>
2023-03-31 23:00:07 -0700

    stylecop editing

--- Spreadsheet_Skyllar_Estill/SpreadsheetTesting/Tests.cs
+++ Spreadsheet_Skyllar_Estill/SpreadsheetTesting/Tests.cs
@@ -14,7 +14,7 @@ namespace SpreadsheetTesting
         /// <summary>
         /// base spreadsheet to use in tests.
         /// </summary>
-        Spreadsheet spreadsheet = new Spreadsheet(10, 10);
+        private Spreadsheet spreadsheet = new Spreadsheet(10, 10);
 
         /// <summary>
         /// test that the spreadsheet constructor has the correct number of rows.
@@ -36,6 +36,9 @@ namespace SpreadsheetTesting
             Assert.That(actualNumCols, Is.EqualTo(10));
         }
 
+        /// <summary>
+        /// tests if text and value get set correctly.
+        /// </summary>
         [Test]
         public void CheckTextandValue()
         {
@@ -43,20 +46,16 @@ namespace SpreadsheetTesting
             Assert.That(this.spreadsheet.GetCell(0, 0).CellValue, Is.EqualTo("10"));
         }
 
+        /// <summary>
+        /// check is cell value of a cell the refereences another changes when the other changes.
+        /// </summary>
         [Test]
         public void CheckChangedCells()
         {
             this.spreadsheet.GetCell(0, 0).CellText = "10";
             this.spreadsheet.GetCell(0, 1).CellText = "=A1";
             this.spreadsheet.GetCell(0, 0).CellText = "12";
-            Assert.That(this.spreadsheet.GetCell(0,1).CellValue, Is.EqualTo("12"));
-        }
-
-        [Test]
-        public void CheckCellExpressions(/*string n, string m, string o, string p, string q*/)
-        {
-            this.spreadsheet.GetCell(5, 8).CellText = "(8+8-2)/7*2";
-            Assert.That(this.spreadsheet.GetCell(1, 1).CellValue, Is.EqualTo("2"));
+            Assert.That(this.spreadsheet.GetCell(0, 1).CellValue, Is.EqualTo("12"));
         }
     }
 }
