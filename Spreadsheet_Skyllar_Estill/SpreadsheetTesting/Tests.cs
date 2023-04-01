0dc8f27b886802c542719dd5a5fe499210695b71
skyllare <skyllarautumn@gmail.com>
2023-03-31 22:54:26 -0700

    updated tests

--- Spreadsheet_Skyllar_Estill/SpreadsheetTesting/Tests.cs
+++ Spreadsheet_Skyllar_Estill/SpreadsheetTesting/Tests.cs
@@ -12,17 +12,18 @@ namespace SpreadsheetTesting
     public class Tests
     {
         /// <summary>
+        /// base spreadsheet to use in tests.
+        /// </summary>
+        Spreadsheet spreadsheet = new Spreadsheet(10, 10);
+
+        /// <summary>
         /// test that the spreadsheet constructor has the correct number of rows.
         /// </summary>
         [Test]
         public void Constructor_CreatesRowsOfCorrectSize()
         {
-            int numRows = 3;
-            int numCols = 0;
-            Spreadsheet spreadsheet = new Spreadsheet(numRows, numCols);
-            int actualNumRows = spreadsheet.RowCount;
-            int actualNumCols = spreadsheet.ColumnCount;
-            Assert.That(actualNumRows, Is.EqualTo(numRows));
+            int actualNumRows = this.spreadsheet.RowCount;
+            Assert.That(actualNumRows, Is.EqualTo(10));
         }
 
         /// <summary>
@@ -31,12 +32,31 @@ namespace SpreadsheetTesting
         [Test]
         public void Constructor_CreatesColumnsOfCorrectSize()
         {
-            int numRows = 0;
-            int numCols = 4;
-            Spreadsheet spreadsheet = new Spreadsheet(numRows, numCols);
-            int actualNumRows = spreadsheet.RowCount;
-            int actualNumCols = spreadsheet.ColumnCount;
-            Assert.That(actualNumCols, Is.EqualTo(numCols));
+            int actualNumCols = this.spreadsheet.ColumnCount;
+            Assert.That(actualNumCols, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void CheckTextandValue()
+        {
+            this.spreadsheet.GetCell(0, 0).CellText = "10";
+            Assert.That(this.spreadsheet.GetCell(0, 0).CellValue, Is.EqualTo("10"));
+        }
+
+        [Test]
+        public void CheckChangedCells()
+        {
+            this.spreadsheet.GetCell(0, 0).CellText = "10";
+            this.spreadsheet.GetCell(0, 1).CellText = "=A1";
+            this.spreadsheet.GetCell(0, 0).CellText = "12";
+            Assert.That(this.spreadsheet.GetCell(0,1).CellValue, Is.EqualTo("12"));
+        }
+
+        [Test]
+        public void CheckCellExpressions(/*string n, string m, string o, string p, string q*/)
+        {
+            this.spreadsheet.GetCell(5, 8).CellText = "(8+8-2)/7*2";
+            Assert.That(this.spreadsheet.GetCell(1, 1).CellValue, Is.EqualTo("2"));
         }
     }
 }
